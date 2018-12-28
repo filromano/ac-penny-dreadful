9953f112c1706b2ee4c89b3e312a0e6e8ef6af04
Filipe Romano Rodrigues <filiperr@br.ibm.com>
2018-12-28 16:23:19 -0200

    adding bottom content

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,45 @@
     <div class="background-page" :style="{'background-image': backgroundImage}">
         <div class="background-gradient">
              <div class="top-content">
-                 <h1>{{title}}</h1>
+                 <h1>{{ title }}</h1>
+                 <p class="details">80% Indicado/Ciencia Ficción/{{ year }}/EUA/14</p>
+             </div>
+             <div class="bottom-content">
+                 <ul class="nav-list">
+                    <li>General</li>
+                    <li>Elenco</li>
+                    <li>Principales Premios</li>
+                 </ul>
+                 <div>
+                     <ul class="actions-list">
+                         <li>
+                             <img src="./assets/add-gray-s.svg" alt="">
+                             <p>Mi Lista</p>
+                        </li>
+                         <li>
+                             <img src="./assets/sad-gray-w.svg" alt="">
+                             <p>Evaluar</p>
+                        </li>
+                         <li><img src="./assets/rec-gray-s.svg" alt=""></li>
+                         <li><img src="./assets/share-gray-s.svg" alt=""></li>
+                     </ul>
+                     <p>Sinopse</p>
+                    <p>{{ sinopse }}</p>
+                 </div>
              </div>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 export default {
     data() {
         return {
             title: '',
             backgroundImage: '',
+            sinopse: '',
+            year: '',
             name: ''
         }
     },
@@ -22,8 +48,10 @@ export default {
         axios.get('https://sample-api-78c77.firebaseio.com/tv-shows/SHOW123.json')
             .then(response => {
                 const serieInfo = response.data
-                this.backgroundImage = 'url(' + serieInfo.Images.Background + ')'
                 this.title = serieInfo.Title
+                this.year = serieInfo.Year
+                this.backgroundImage = 'url(' + serieInfo.Images.Background + ')'
+                this.sinopse = serieInfo.Synopsis
                 console.log(response)
                 const castData = serieInfo.Cast
                 const cast = []
@@ -51,31 +79,71 @@ export default {
         background: transparent;
     }
 
-    %full-size{
+    %full-size {
         width: 100%;
         height: 100%;
     }
-    body, html{
+    body, html {
         @extend %full-size;
         font-family: 'Nunito', sans-serif;
         color: white;
     }
 
-    .background-page{
+    .background-page {
         @extend %full-size;
         background-size: cover;
+        position: relative;
     }
-    .background-gradient{
+    .background-gradient {
         @extend %full-size;
         background: linear-gradient(to left,
                      rgba(0,0,0, 1), 
-                     rgba(0,0,0, .5));
+                     rgba(0,0,0, .5)),
+                    linear-gradient(to top,
+                     rgba(0,0,0, .4), 
+                     rgba(0,0,0, 0),
+                     rgba(0,0,0, .4));
     }
-    .top-content{
-        padding: 20px;
+    .top-content {
+        padding: 20px 40px;
     }
     h1{
         font-size: 55px;
-        font-weight: 300px;
+        font-weight: 'Bold';
+    }
+    .details {
+        font-size: 20px;
+        color: #C7C7C7;
+    }
+    .bottom-content {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 30%;
+        background: linear-gradient(to right,
+                     rgba(25, 25, 25, 1),
+                     rgba(25, 25, 25, .9),
+                     rgba(0,0,0, 0),
+                     rgba(0,0,0, 0));
+        padding: 20px 40px;
+    }
+
+    .nav-list{
+        list-style: none;
+        display: inline-block;
+        clear: both;
+        li {
+            float: left;
+        }
+    }
+
+    .actions-list {
+        list-style: none;
+        display: inline-block;
+        clear: both;
+        li {
+            float: left;
+            margin-left: 10px;
+        }
     }
 </style>
