3ade22bec1098a50f9d83dfdd37890d96eca8e55
Filipe Romano Rodrigues <filiperr@br.ibm.com>
2019-01-03 15:49:12 -0200

    fix paths

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/penny-dreadful-filipe/dist/',
     filename: 'build.js'
   },
   module: {
